78a1b5f7703164b231a6c8b9ac48e267a6891d4a
Jalaj <96870071+jalaj711@users.noreply.github.com>
2023-12-26 16:11:33 +0530

    added subject type
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

---
+++ lib/types/subject_type.dart
@@ -0,0 +1,9 @@
+class Subject {
+
+  const Subject({required this.title, required this.target, required this.id, required this.attended, required this.total_classes});
+  final String title;
+  final int target;
+  final int id;
+  final int attended;
+  final int total_classes;
+}
